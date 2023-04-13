d75b16cbe578cd5339f0ed2e74ec390333f8e8c3
Lada Starodubtseva <e2038512@cmaisonneuve.qc.ca>
2023-04-13 09:57:00 -0400

    s10 début du carrousel

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 4w4-carrousel
+# carrousel
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,15 +1,11 @@
 (function(){
-    console.log('Début du carrousel')
+    console.log('début carrousel')
     let boutcarrousel__ouvrir = document.querySelector('.carrousel__ouvrir');
     let carrousel = document.querySelector('.carrousel');
     let carrousel__x = document.querySelector('.carrousel__x');
-
-
     boutcarrousel__ouvrir.addEventListener('mousedown',function(){
         carrousel.classList.add('carrousel--ouvrir')
-    }) 
-
-
+    })
     carrousel__x.addEventListener('mousedown',function(){
         carrousel.classList.remove('carrousel--ouvrir')
     })
